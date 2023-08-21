61c384af253bcfce3f8913a41b4c3244e67da4dd
douglas.aquino <dassmxjf@gmail.com>
2023-08-21 04:32:38 -0300

    ajuste

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,63 +1,70 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Calculadora de Horas</title>
-    <link rel="stylesheet" href="{{asset('css/estiloBasico.css')}}" type="text/css" href="style.css">
+    <link rel="stylesheet" href="{{ asset('css/estiloBasico.css') }}" type="text/css" href="style.css">
 </head>
+
 <body>
     <h1>Calculadora de Horas</h1>
     <form action={{ route('site.principal') }} method="post">
         @csrf {{--  token laravel para post  --}}
         <label for="hora_inicial">Hora Inicial:</label>
         <input type="time" name="hora_inicial" required><br>
-        
+
         <label for="hora_final">Hora Final:</label>
         <input type="time" name="hora_final" required><br>
-        
+
         <input type="submit" value="Calcular">
     </form>
 </body>
+
 </html>
 
 <?php
+
 $hora_inicial = $_POST['hora_inicial'];
 $hora_final = $_POST['hora_final'];
 
-// Converte as horas para o formato de timestamp
-$timestamp_inicial = strtotime($hora_inicial);
-$timestamp_final = strtotime($hora_final);
+// Função para converter hora em minutos
+function converterParaMinutos($hora)
+{
+    [$horas, $minutos] = explode(':', $hora);
+    return $horas * 60 + $minutos;
+}
 
-// Calcula a diferença em segundos
-$diferenca_segundos = $timestamp_final - $timestamp_inicial;
+// Função para converter minutos em horas
+function converterParaHoras($minutos)
+{
+    return floor($minutos / 60) + ($minutos % 60) / 100;
+}
 
-// Converte a diferença para horas
-$horas_trabalhadas = $diferenca_segundos / 3600;
+// Converte as horas para minutos
+$minutos_inicial = converterParaMinutos($hora_inicial);
+$minutos_final = converterParaMinutos($hora_final);
 
 // Regras de negócios
-$horas_diurnas = 0;
-$horas_noturnas = 0;
+$minutos_diurnos = 0;
+$minutos_noturnos = 0;
 
-if ($horas_trabalhadas > 0) {
-    $hora_inicio_diurno = strtotime('05:00');
-    $hora_fim_diurno = strtotime('22:00');
+$inicio_periodo_noturno = converterParaMinutos('22:00');
+$inicio_periodo_diurno = converterParaMinutos('05:00');
 
-    if ($timestamp_inicial >= $hora_fim_diurno || $timestamp_final <= $hora_inicio_diurno) {
-        // Horas noturnas
-        $horas_noturnas = $horas_trabalhadas;
-    } elseif ($timestamp_inicial >= $hora_inicio_diurno && $timestamp_final <= $hora_fim_diurno) {
-        // Horas diurnas
-        $horas_diurnas = $horas_trabalhadas;
-    } else {
-        // Parte diurna e parte noturna
-        if ($timestamp_inicial < $hora_inicio_diurno) {
-            $horas_noturnas += ($hora_inicio_diurno - $timestamp_inicial) / 3600;
-        }
-        if ($timestamp_final > $hora_fim_diurno) {
-            $horas_noturnas += ($timestamp_final - $hora_fim_diurno) / 3600;
-        }
-        $horas_diurnas = $horas_trabalhadas - $horas_noturnas;
+
+    if ($minutos_inicial < $inicio_periodo_diurno) {
+        $minutos_noturnos += $inicio_periodo_diurno - $minutos_inicial;
+        $minutos_diurnos = $minutos_final - $minutos_inicial - $minutos_noturnos;
+
     }
-}
+    if ($minutos_final > $inicio_periodo_noturno) {
+        $minutos_noturnos += $minutos_final - $inicio_periodo_noturno;
+        $minutos_diurnos = $minutos_final - $minutos_inicial - $minutos_noturnos;
+    }
+
+// Converter minutos para horas
+$horas_diurnas = converterParaHoras($minutos_diurnos);
+$horas_noturnas = converterParaHoras($minutos_noturnos);
 
 // Exibir resultados
 echo "Horas Diurnas: $horas_diurnas<br>";
