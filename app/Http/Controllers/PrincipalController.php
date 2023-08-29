d5ea4bb297fd3d29c49deaffa5e2220798c8a59b
douglas.aquino <dassmxjf@gmail.com>
2023-08-29 18:55:27 -0300

    ajuste mvc, calculo para controler

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -7,8 +7,68 @@ use Illuminate\Http\Request;
 
 class PrincipalController extends Controller
 {
-    public function principal()
+    public function principal(Request $request)
     {;
-        return view('site.principal');
+        $dados = $request->all(); //recebe horas
+
+        if (isset($dados)) {
+
+            // Converte as horas para minutos
+
+            [$horas, $minutos] = explode(':', $dados['hora_inicial']);
+            $minutos_inicial = $horas * 60 + $minutos;
+
+
+            [$horas, $minutos] = explode(':', $dados['hora_final']);
+            $minutos_final = $horas * 60 + $minutos;
+
+
+            // Regras de negócios
+            $minutos_diurnos = 0;
+            $minutos_noturnos = 0;
+
+            [$horas, $minutos] = explode(':', "22:00");
+            $inicio_periodo_noturno = $horas * 60 + $minutos;
+
+            [$horas, $minutos] = explode(':', "05:00");
+            $inicio_periodo_diurno = $horas * 60 + $minutos;
+
+            $i = $minutos_inicial;
+            while ($i != $minutos_final) {
+                if ($i >= $minutos_inicial) {
+                    if ($i == $minutos_final) {
+                        return;
+                    }
+                    if ($i >= 1440) {
+                        $i = 0;
+                        $minutos_noturnos++;
+                    }
+                    if ($i < $inicio_periodo_noturno && $i != 0) {
+                        $minutos_diurnos++;
+                    }
+                    if ($i >= $inicio_periodo_noturno) {
+                        $minutos_noturnos++;
+                    }
+                } else {
+                    if ($i >= $inicio_periodo_diurno) {
+                        $minutos_diurnos++;
+                    } else {
+                        $minutos_noturnos++;
+                    }
+                }
+                $i++;
+            }
+
+            // Converter minutos para horas
+            $horas_diurnas = floor($minutos_diurnos / 60) + ($minutos_diurnos % 60) / 100;
+
+            $horas_noturnas = floor($minutos_noturnos / 60) + ($minutos_noturnos % 60) / 100;
+
+            return view('site.principal',['horas_diurnas' => $horas_diurnas, 'horas_noturnas' => $horas_noturnas]);
+
+            // Exibir resultados
+        } else {
+            return view('site.principal');
+        }
     }
 }
--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -18,70 +18,11 @@
 
         <input type="submit" value="Calcular">
     </form>
-</body>
-
-</html>
-
-<?php
-
-$hora_inicial = $_POST['hora_inicial'];
-$hora_final = $_POST['hora_final'];
-
-// Função para converter hora em minutos
-function converterParaMinutos($hora)
-{
-    [$horas, $minutos] = explode(':', $hora);
-    return $horas * 60 + $minutos;
-}
-
-// Função para converter minutos em horas
-function converterParaHoras($minutos)
-{
-    return floor($minutos / 60) + ($minutos % 60) / 100;
-}
-
-        // Converte as horas para minutos
-        $minutos_inicial = converterParaMinutos($hora_inicial);
-        $minutos_final = converterParaMinutos($hora_final);
-
-        // Regras de negócios
-        $minutos_diurnos = 0;
-        $minutos_noturnos = 0;
-
-        $inicio_periodo_noturno = converterParaMinutos("22:00");
-        $inicio_periodo_diurno = converterParaMinutos("05:00");
-
-            $i = $minutos_inicial;
-            while ($i != $minutos_final) {
-                if ($i >= $minutos_inicial) {
-                    if ($i == $minutos_final) {
-                        return;
-                    }
-                    if ($i >= 1440) {
-                        $i = 0;
-                        $minutos_noturnos++;
-                    }
-                    if ($i < $inicio_periodo_noturno && $i != 0) {
-                        $minutos_diurnos++;
-                    }
-                    if ($i >= $inicio_periodo_noturno) {
-                        $minutos_noturnos++;
-                    }
-                } else {
-                    if ($i >= $inicio_periodo_diurno) {
-                        $minutos_diurnos++;
-                    } else {
-                        $minutos_noturnos++;
-                    }
-                }
-                $i++;
-            }
-
-        // Converter minutos para horas
-        $horas_diurnas = converterParaHoras($minutos_diurnos);
-        $horas_noturnas = converterParaHoras($minutos_noturnos);
     
-// Exibir resultados
-echo "Horas Diurnas: $horas_diurnas<br>";
-echo "Horas Noturnas: $horas_noturnas";
-?>
+    @if(isset($horas_diurnas) && isset($horas_noturnas))
+    <p>Horas Diurnas: {{ $horas_diurnas }}
+    <p>Horas Noturnas: {{ $horas_noturnas }}
+    @endif
+
+</body>
+</html>
